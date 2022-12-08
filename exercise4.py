80e72a28fcfba64da273cbde5a69ba9f584c24cd
Melon-Lord-07 <nitchis@educ.college-montreal.qc.ca>
2022-12-08 09:12:18 -0500

    exercises progres sur exercise 4

--- exercise4.py
+++ exercise4.py
@@ -1,1 +1,14 @@
+"""
+Sandra Nitchi, 2022
+exercise 3
+...
+
+"""
+
+class Hero:
+    def __init__(self, nom_hero, points_vie, force_attaque, force_defense):
+        self.nom_hero = nom_hero
+        self.points_vie = points_vie
+        self.force_attaque = force_attaque
+        self.force_defense = force_defense
 
